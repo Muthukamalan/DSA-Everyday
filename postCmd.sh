981b68a1e019f1c5c88f9ce506aa912b9ec2e67f
Muthu Kamalan <muthukamalan98@gmail.com>
2025-09-24 14:07:32 +0000

    fix hook and postCmd.sh

--- postCmd.sh
+++ postCmd.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-apt update && apt install python3 python3.13-venv
+apt update && apt install python3 python3.13-venv -y
+python3 -m venv .venv
+source .venv/bin/activate
+pip install pre-commit
